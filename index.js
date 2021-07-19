1e62812fe1f3700d00aacd4a6cc9dc99b20ee5eb
jpoulin587 <jpoulin587a@gmail.com>
2021-07-19 14:35:07 -0500

    added real questions and upteted readme template

--- index.js
+++ index.js
@@ -20,43 +20,44 @@ inquirer
         },
         {
             type: 'input',
-            name: 'placeholderName',
-            message: 'This is a placeholder message',
+            name: 'usage',
+            message: 'What command do you use to launch this app?',
         },
         {
             type: 'input',
-            name: 'placeholderName',
-            message: 'This is a placeholder message',
+            name: 'credits',
+            message: 'List all the people who worked on this app.',
         },
         {
             type: 'input',
-            name: 'placeholderName',
-            message: 'This is a placeholder message',
+            name: 'contributing',
+            message: 'If someone wants to contribute to this app, what should they do?',
         },
         {
             type: 'input',
-            name: 'placeholderName',
-            message: 'This is a placeholder message',
+            name: 'tests',
+            message: 'Are there any tests the user can users can run to try out your app?',
         },
         {
             type: 'input',
-            name: 'placeholderName',
+            name: 'questions',
+            message: 'Enter your email so users can contact you with questions.',
+        },
+        {
+            type: 'checkbox',
+            name: 'licenseSelect',
             message: 'This is a placeholder message',
+            choices: ['MIT', 'CSS', 'JavaScript', 'MySQL'],
         },
         {
             type: 'input',
-            name: 'placeholderName',
-            message: 'This is a placeholder message',
+            name: 'copyrightYear',
+            message: 'What is the year you created this app?',
         },
         {
             type: 'input',
-            name: 'placeholderName',
-            message: 'This is a placeholder message',
-        },
-        {
-            type: 'input',
-            name: 'placeholderName',
-            message: 'This is a placeholder message',
+            name: 'CopyrightName',
+            message: 'What name should the copyright for this app belong to?',
         },
         
         
