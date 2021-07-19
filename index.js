0b496c56d5391698fd579b6fb9a39d3d54fe2547
jpoulin587 <jpoulin587a@gmail.com>
2021-07-19 18:32:41 -0500

    added templates and generates readme

--- index.js
+++ index.js
@@ -1,5 +1,56 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+let chosenLicense
+
+const generateReadme = (data) =>
+    `
+    # Project Title 
+    ${data.projectTitle}
+    
+    ## Description
+    ${data.description}
+    
+    ## Table of Contents
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+    - [License](#license)
+    
+    
+    ## Instillation
+    You can activate this app by ${data.installation}
+    
+    ## Usage
+    Stock usage text -  ${data.usage} 
+    
+    ## Contributing
+    Stock usage text -  ${data.usage}
+    
+    ## Tests
+    Stock usage text -  ${data.test} 
+    
+    ## Questions
+    Stock usage text -  ${data.questions} 
+    
+    ## License 
+    Copyright ${data.copyrightYear}  ${data.copyrightName}
+    ${chosenLicense}
+    
+    
+    `; // end of the template
+
+// License templates
+let licMit = `Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
+
+
+
 
 inquirer
     .prompt([
@@ -44,10 +95,10 @@ inquirer
             message: 'Enter your email so users can contact you with questions.',
         },
         {
-            type: 'checkbox',
+            type: 'list',
             name: 'licenseSelect',
             message: 'This is a placeholder message',
-            choices: ['MIT', 'CSS', 'JavaScript', 'MySQL'],
+            choices: ['MIT', 'Apache', 'GNU',],
         },
         {
             type: 'input',
@@ -56,18 +107,35 @@ inquirer
         },
         {
             type: 'input',
-            name: 'CopyrightName',
+            name: 'copyrightName',
             message: 'What name should the copyright for this app belong to?',
         },
-        
-        
+
+
 
     ])
 
     .then((data) => {
         console.log(data);
-
         console.log(data.projectTitle);
+        const writeReadme = generateReadme(data);
 
+        switch (data.license) {
+            case 'MIT':
+                chosenLicense = licMit
+                break;
+                case 'Apache':
+                chosenLicense = licApache
+                break;
+                case 'GNU':
+                chosenLicense = licGnu
+                break;
+        }
+
+        const filename = `${data.projectTitle.toLowerCase().split(' ').join('')}.md`;
+
+        fs.writeFile(filename, writeReadme, (err) =>
+            err ? console.log(err) : console.log('Success!')
+        );
 
     });
